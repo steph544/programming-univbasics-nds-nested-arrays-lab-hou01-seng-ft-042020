13d19b9542384d8053dd1687f3e648f7f2a07b52
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-20 23:01:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,10 +16,11 @@ CONVENTIONAL_PRODUCE = [
   "Eggplant"
 ]
 
-def assembled_matrix
+def assembled_matrix =[
   produce=[
    conventional_produce = ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"]
    organic_produce = ["Strawberries","Potatoes","Grapes","Avocadoes","Asparagus"  ]
+   ]
    ]
 end
 
